66d7e82ace374f90752ca7cedd64da26f542252b
krasi <vechesumstudent@gmail.com>
2022-08-15 20:59:14 +0300

    Extracted some constants

--- project/functions/constants.R
+++ project/functions/constants.R
@@ -1,6 +1,9 @@
 simulation_factor_loadings_description <- c(
-  "Data where 1 variable has strong connection with the response variable",
-  "Data where 1/3 of the dataset has correlation and stronger connection with the response variable",
-  "Data where all variables are correlated with the response variable",
-  "Data with no leading/strong factor"
+  "Case I: Data where 1 variable has strong connection with the response variable",
+  "Case II: Data where 1/3 of the dataset has correlation and stronger connection with the response variable",
+  "Case III: Data where all variables are correlated",
+  "Case IV: Data with no leading/strong factor"
 )
+number_of_factors_possible_values <- c(1, 6)
+c_tilde_possible_values <- c(1,4,9)
+case_numbers <- c(1, 2, 3)
