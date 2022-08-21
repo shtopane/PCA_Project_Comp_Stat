5a263769a9e6e883325569ba8590cdbe6132eded
krasi <vechesumstudent@gmail.com>
2022-08-21 16:56:59 +0300

    Created constants to use for application, got training set

--- project/functions/helpers.R
+++ project/functions/helpers.R
@@ -1,4 +1,4 @@
-source("functions/constants.R")
+source("project/functions/constants.R")
 
 get_percentage_of_number <- function(percentage, number){
   return(round(percentage / number, digits = 2))
@@ -81,4 +81,15 @@ plot_MSE_comparison_boxplot <- function(MSE_list, MSE_stats_list){
   
   return(plots)
   
+}
+
+string_date_to_vector_date <- function(string_date){
+  
+  # parse string to number
+  result <- as.numeric(
+    # split the string by -, get only numbers. [[1]] extracts the year, month, day
+    strsplit(string_date, "-")[[1]]
+  )[-3] # this removes the day. So, we'll get c(1971, 1) and not c(1971, 10, 1)
+  
+  return(result)
 }
