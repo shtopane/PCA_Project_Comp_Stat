9c9d554a34b79ec4ec5b7332db5457fa451f8ae9
krasi <vechesumstudent@gmail.com>
2022-08-22 18:22:13 +0300

    added bar plot for simulation 4(without ncomp)

--- project/functions/run_simulations.R
+++ project/functions/run_simulations.R
@@ -1,18 +1,23 @@
-run_simulation8 <- function(simulation_option_default, dgp_option_default){
-  sim_option <- simulation_option_default 
-  sim_option$N <- 192
-  sim_option$p <- 108
-  sim_option$runs <- 1000
-  sim_option$ncomp <- 6
+run_simulation8 <- function(simulation_option_default, dgp_option_default) {
+  simulation8 <- new.env()
   
-  # result <- simulation(dgp_option_list = dgp_option_default, simulation_option_list = sim_option)
+  simulation8$sim_option <- simulation_option_default
+  simulation8$sim_option$N <- 192
+  simulation8$sim_option$p <- 108
+  simulation8$sim_option$runs <- 1000
+  simulation8$sim_option$ncomp <- 6
+  
+  simulation8$result <-
+    simulation(dgp_option_list = dgp_option_default,
+               simulation_option_list = simulation8$sim_option)
   
   save(simulation8, file = "simulation_results_Rdata/simulation8.RData")
   
-  MSE_stats <- save_mean_mse_and_return_stats(result)
+  simulation8$MSE_stats <-
+    save_mean_mse_and_return_stats(simulation8$result)
   
   pdf(file = "images/simulation_results/simulation8.pdf")
   par(mfrow = c(3, 2))
-  plot_MSE_comparison_boxplot(result , MSE_stats)
+  plot_MSE_comparison_boxplot(simulation8$result , simulation8$MSE_stats)
   dev.off()
 }
